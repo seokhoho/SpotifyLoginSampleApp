40b097d1fde56d4ee7102e98d92efbab79e5a5ce
seokhoho <kreel9402@gmail.com>
2022-09-08 05:16:14 +0900

    Build Finish
    Apple LogOut, .createProfileChangeReques use

--- SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/MainViewController.swift
@@ -52,4 +52,18 @@ class MainViewController: UIViewController {
         let email = Auth.auth().currentUser?.email ?? ""
         Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
     }
+    
+    @IBAction func profileUpdateButtonTapped(_ sender: UIButton) {
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = "코끼리"
+        changeRequest?.commitChanges { _ in
+            let displayName = Auth.auth().currentUser?.displayName
+            ?? Auth.auth().currentUser?.email ?? "고객"
+            
+            self.welcomeLabel.text = """
+                환영합니다.
+                \(displayName)님
+                """
+        }
+    }
 }
